8e8e3e6f3ec8aff2dfff0a37a2aa2b7f24186fe9
Gunhee <nca2.student12@ncakocca.kr>
2026-07-13 14:32:46 +0900

    260713_00

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -5,6 +5,7 @@ export const CHARACTER_HEAD = assetUrl('images/characters/head.png')
 export const CHARACTER_BODY = assetUrl('images/characters/body.png')
 export const GROOM_HAIR = assetUrl('images/characters/man_hair_00.png')
 export const BRIDE_HAIR = assetUrl('images/characters/woman_hair_00.png')
+export const BRIDE_HAIR_01 = assetUrl('images/characters/woman_hair_01.png')
 export const GROOM_SUIT = assetUrl('images/characters/suit00.png')
 export const BRIDE_DRESS = assetUrl('images/characters/dress00.png')
 
@@ -33,17 +34,17 @@ export interface OutfitOption {
 export const FACE_EXPRESSIONS: FaceExpr[] = [
   { id: 'face00', name: '기본', image: assetUrl('images/characters/expr/face00.png'), price: 0 },
   { id: 'face01', name: '뾰로통', image: assetUrl('images/characters/expr/face01.png'), price: 500_000 },
-  { id: 'face02', name: '표정 2', image: assetUrl('images/characters/expr/face02.png'), price: 500_000 },
-  { id: 'face03', name: '표정 3', image: assetUrl('images/characters/expr/face03.png'), price: 500_000 },
-  { id: 'face04', name: '표정 4', image: assetUrl('images/characters/expr/face04.png'), price: 500_000 },
-  { id: 'face05', name: '표정 5', image: assetUrl('images/characters/expr/face05.png'), price: 500_000 },
-  { id: 'face06', name: '표정 6', image: assetUrl('images/characters/expr/face06.png'), price: 500_000 },
-  { id: 'face07', name: '표정 7', image: assetUrl('images/characters/expr/face07.png'), price: 500_000 },
-  { id: 'face08', name: '표정 8', image: assetUrl('images/characters/expr/face08.png'), price: 500_000 },
-  { id: 'face09', name: '표정 9', image: assetUrl('images/characters/expr/face09.png'), price: 500_000 },
-  { id: 'face10', name: '표정 10', image: assetUrl('images/characters/expr/face10.png'), price: 500_000 },
-  { id: 'face11', name: '표정 11', image: assetUrl('images/characters/expr/face11.png'), price: 500_000 },
-  { id: 'face12', name: '표정 12', image: assetUrl('images/characters/expr/face12.png'), price: 500_000 },
+  { id: 'face02', name: '엉엉', image: assetUrl('images/characters/expr/face02.png'), price: 500_000 },
+  { id: 'face03', name: '부끄', image: assetUrl('images/characters/expr/face03.png'), price: 500_000 },
+  { id: 'face04', name: '새침', image: assetUrl('images/characters/expr/face04.png'), price: 500_000 },
+  { id: 'face05', name: '윙크', image: assetUrl('images/characters/expr/face05.png'), price: 500_000 },
+  { id: 'face06', name: '시무룩', image: assetUrl('images/characters/expr/face06.png'), price: 500_000 },
+  { id: 'face07', name: '반짝', image: assetUrl('images/characters/expr/face07.png'), price: 500_000 },
+  { id: 'face08', name: '하트', image: assetUrl('images/characters/expr/face08.png'), price: 500_000 },
+  { id: 'face09', name: '뿌듯', image: assetUrl('images/characters/expr/face09.png'), price: 500_000 },
+  { id: 'face10', name: '기대', image: assetUrl('images/characters/expr/face10.png'), price: 500_000 },
+  { id: 'face11', name: '활짝', image: assetUrl('images/characters/expr/face11.png'), price: 500_000 },
+  { id: 'face12', name: '콕', image: assetUrl('images/characters/expr/face12.png'), price: 500_000 },
 ]
 
 // 기본(base) 상태의 표정.
@@ -62,11 +63,22 @@ export const CHARACTERS: { key: CharacterKey; label: string }[] = [
 export const HAIR_OPTIONS: Record<CharacterKey, HairOption[]> = {
   groom: [
     { id: DEFAULT_HAIR_ID, name: '헤어 없음', image: null, price: 0 },
-    { id: 'man_hair_00', name: '남자 헤어 1', image: GROOM_HAIR, price: 500_000 },
+    { id: 'man_hair_00', name: '신랑 헤어 1', image: GROOM_HAIR, price: 500_000 },
   ],
   bride: [
     { id: DEFAULT_HAIR_ID, name: '헤어 없음', image: null, price: 0 },
-    { id: 'woman_hair_00', name: '여자 헤어 1', image: BRIDE_HAIR, price: 500_000 },
+    { id: 'woman_hair_00', name: '신부 헤어 1', image: BRIDE_HAIR, price: 500_000 },
+    { id: 'woman_hair_01', name: '신부 헤어 2', image: BRIDE_HAIR_01, price: 500_000 },
+    { id: 'woman_hair_02', name: '신부 헤어 3', image: assetUrl('images/characters/woman_hair_02.png'), price: 500_000 },
+    { id: 'woman_hair_03', name: '신부 헤어 4', image: assetUrl('images/characters/woman_hair_03.png'), price: 500_000 },
+    { id: 'woman_hair_04', name: '신부 헤어 5', image: assetUrl('images/characters/woman_hair_04.png'), price: 500_000 },
+    { id: 'woman_hair_05', name: '신부 헤어 6', image: assetUrl('images/characters/woman_hair_05.png'), price: 500_000 },
+    { id: 'woman_hair_06', name: '신부 헤어 7', image: assetUrl('images/characters/woman_hair_06.png'), price: 500_000 },
+    { id: 'woman_hair_07', name: '신부 헤어 8', image: assetUrl('images/characters/woman_hair_07.png'), price: 500_000 },
+    { id: 'woman_hair_08', name: '신부 헤어 9', image: assetUrl('images/characters/woman_hair_08.png'), price: 500_000 },
+    { id: 'woman_hair_11', name: '신부 헤어 12', image: assetUrl('images/characters/woman_hair_11.png'), price: 500_000 },
+    { id: 'woman_hair_13', name: '신부 헤어 14', image: assetUrl('images/characters/woman_hair_13.png'), price: 500_000 },
+    { id: 'woman_hair_17', name: '신부 헤어 18', image: assetUrl('images/characters/woman_hair_17.png'), price: 500_000 },
   ],
 }
 
